9904459a5caf58c152d7699c39590cdbb028cd4c
Michael.Shieh <michael2008s@gmail.com>
2018-04-27 22:06:29 +0800

    add example

--- examples/xmrhttp.go
+++ examples/xmrhttp.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"github.com/GoChainRpc/xmrrpc"
+	"github.com/GoChainRpc/xmrrpc/xmrjson"
 )
 
 func main() {
@@ -37,6 +38,25 @@ func main() {
 		log.Fatal("GetTransfers_err:", err)
 	} else {
 		log.Println("transfersResult:", transfersResult)
+	}
+
+	transferDestinations := []xmrjson.TransferDestination{}
+	transferDestination := xmrjson.TransferDestination{}
+	transferDestination.Address = "46fPzT8eJGZdkHuoQSXQSLK8yDFiDLPt4WeRMk5BA87SB7fmrurK2woB1RF9TdmNuUdmp9ZzTEgUMTCWBQiP3SUC2NVZ7Sb"
+	transferDestination.Amount = 1256000000
+	transferDestinations = append(transferDestinations,transferDestination)
+
+	transferDestination2 := xmrjson.TransferDestination{}
+	transferDestination2.Address = "46fPzT8eJGZdkHuoQSXQSLK8yDFiDLPt4WeRMk5BA87SB7fmrurK2woB1RF9TdmNuUdmp9ZzTEgUMTCWBQiP3SUC2NVZ7Sb"
+	transferDestination2.Amount = 1113000000
+	transferDestinations = append(transferDestinations,transferDestination2)
+
+	transferResult ,err := client.Transfer(transferDestinations,0,"a6511848208d6e54893cb77e33368d9c245d41e6ee9d6d5e31b85bd51179ee1a")
+
+	if err != nil {
+		log.Fatal(err)
+ 	} else {
+ 		log.Println(transferResult)
 	}
 
 }
